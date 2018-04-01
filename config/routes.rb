3850cde8674eb3511d0057bb0bf5b5bddb1bfa79
witoldzol <blue3656@gmail.com>
2018-04-01 15:29:46 +0200

    rolled back scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  resources :products
 	# controller - method
 	root 'pages#home'
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180401131720) do
-
-  create_table "products", force: :cascade do |t|
-    t.string "title"
-    t.text "description"
-    t.decimal "price"
-    t.string "image"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
